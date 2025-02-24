9df4c3b66be0db3b309e54bcb4864eb5404f1723
OtoBourbon <sharvashidze0712@gmail.com>
2025-02-24 21:06:23 +0400

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,14 @@
 import { useApi } from "../contexts/ApiContext";
 import WeatherImage from "./WeatherImage";
+import { motion } from "motion/react";
 
 export default function Home() {
   const { cityWeather } = useApi();
   return (
     <section className="weather">
-      <div>
+      <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
         <span className="weather-city">{cityWeather.city}</span>
-      </div>
+      </motion.div>
       <div className="weather-data">
         <WeatherImage sky={cityWeather.sky} />
         <span className="weather-data-degree">
