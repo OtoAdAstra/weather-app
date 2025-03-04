86209e34ae2d96286b056a4aca7db198cab02f19
OtoBourbon <sharvashidze0712@gmail.com>
2025-03-04 22:41:07 +0400

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import { useApi } from "../contexts/ApiContext";
 import WeatherImage from "./WeatherImage";
 import { motion } from "motion/react";
 
 export default function Home() {
   const { cityWeather } = useApi();
+  const [details, setDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setDetails((prev) => !prev);
+  };
+
+  console.log(details);
+
   return (
     <section className="weather">
-      <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+      <motion.div
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.8 }}
+        onClick={toggleDetails}>
         <span className="weather-city">{cityWeather.city}</span>
       </motion.div>
-      <div className="weather-data">
+
+      <motion.div
+        className="weather-data"
+        transition={{ duration: 0.5 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: details ? 1 : 0 }}
+        style={{ display: "flex" }}>
         <WeatherImage sky={cityWeather.sky} />
         <span className="weather-data-degree">
           {(cityWeather.temp - 273.15).toFixed()} °C
         </span>
-      </div>
+      </motion.div>
     </section>
   );
 }
